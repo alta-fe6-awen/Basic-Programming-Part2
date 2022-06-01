7eb234359d60be0f56c9f465fc7d646551846d1c
Awen <awendml@gmail.com>
2022-06-02 07:46:08 +0800

    Poblem1 ditambahkan

--- Part2/problem1/problem1.js
+++ Part2/problem1/problem1.js
@@ -1,7 +1,18 @@
 // input
 let studentScore = 80;
 
-// Process: Your Solution Code Here
-
+	if (studentScore >= 80 && studentScore <= 100) {
+		console.log("Nilai A");
+	}else if (studentScore >= 65 && studentScore <= 79) {
+		console.log("Nilai B+");
+	}else if (studentScore >= 50 && studentScore <= 64) {
+		console.log("Nilai B");
+	}else if (studentScore >= 35 && studentScore <= 49) {
+		console.log("Nilai C");
+	}else if (studentScore >= 0 && studentScore <= 34) {
+		console.log("Nilai D");
+	}else {
+		console.log("Invalid");
+	}
 // Output
 // Nilai A
